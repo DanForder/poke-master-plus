51dab7fad48e45d78f715c32d6434e388ee943b6
Dan Forder <dan.forder@Nology.local>
2019-08-15 14:00:32 +0100

    display changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ import { Link } from "@reach/router";
 class App extends Component {
   render() {
     return (
-      <div class="wrapper">
-        <header class="header">
-          <h1>pokedex.dforder.com</h1>
-        </header>
-        <Routes />
-        <nav class="navbar">
-          <div>
-            <Link to="/home">Home</Link>
-          </div>
-          <div>
-            <Link to="/pokedex">The Pokedex</Link>
-          </div>
-        </nav>
+      <div class="container">
+        <div class="wrapper">
+          <header class="header">
+            <h1>pokedex.dforder.com</h1>
+          </header>
+          <Routes />
+          <nav class="navbar">
+            <div>
+              <Link to="/home">Home</Link>
+            </div>
+            <div>
+              <Link to="/pokedex">The Pokedex</Link>
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
