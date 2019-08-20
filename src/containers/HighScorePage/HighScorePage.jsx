4082650f7e4ff78c02cb03a4ea42efaa3caa001f
Dan Forder <dan.forder@Nology.local>
2019-08-20 13:21:33 +0100

    sorted highscores

--- src/containers/HighScorePage/HighScorePage.jsx
+++ src/containers/HighScorePage/HighScorePage.jsx
@@ -4,13 +4,17 @@ import styles from "./HighScorePage.module.scss";
 class HighScorePage extends Component {
   state = { highScores: [] };
 
+  sortHighScores = (a, b) => {
+    return b[1] - a[1];
+  };
+
   render() {
     return (
       <main className={styles.wrapper}>
         <h2>High Scores</h2>
         <div>
           {this.props.highScores.length > 0 ? (
-            this.props.highScores.map(highScore => {
+            this.props.highScores.sort(this.sortHighScores).map(highScore => {
               return (
                 <div className={styles.highScore}>
                   <p>{highScore[0]}</p>
